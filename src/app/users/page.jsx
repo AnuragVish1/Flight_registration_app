a53d13810a87a1745ded088d1c83accc773262d5
Anurag <anuragvishwakarmaalt@gmail.com>
2024-04-24 20:24:54 +0530

    ok

---
+++ src/app/users/page.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function login() {
+  return (
+    <div className="Border_container">
+      <div className="Container">
+        <div className="form_page">
+          <div className="title">
+            <h3>BooknFly</h3>  
+          <div className="details">
+            <h2>Login</h2>
+            <label></label>
+          </div>
+        </div>
+        <div className="image">
+
+        </div>
+
+      </div>
+
+    </div>
+    </div>
+  )
+}
+
+export default login
