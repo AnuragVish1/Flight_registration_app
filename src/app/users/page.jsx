1d6ffcf787801b35d404580948865ce42f82663e
Anurag <anuragvishwakarmaalt@gmail.com>
2024-04-25 01:24:12 +0530

    ok

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,25 +1,39 @@
+
+import Link from 'next/link'
 import React from 'react'
+import styles from '../page.module.css'
 
 function login() {
   return (
+    
+    <div className="Main_container">
     <div className="Border_container">
       <div className="Container">
+        <div className="form_Container">
         <div className="form_page">
+          <div className="form_login">
           <div className="title">
-            <h3>BooknFly</h3>  
-          <div className="details">
-            <h2>Login</h2>
-            <label></label>
+            <h2>BooknFly</h2>  
+            </div>
+          <div className="detail">
+            
+            <label className="email">Email</label>
+            <input type="email" placeholder="Enter your email" />
+            <label className="password">Password</label>
+            <input type="password" placeholder="Enter your password" />
+            <button className="login_btn">Login</button>
+            <p>Don't have an account? <Link href="/registration">Sign Up</Link></p>
           </div>
         </div>
-        <div className="image">
-
-        </div>
-
+        
       </div>
-
+    
+    <div className="image_holder"></div>
     </div>
     </div>
+    </div>
+    </div>
+  
   )
 }
 
